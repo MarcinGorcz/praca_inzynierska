3bba67f420b0bf4d5a2cf91a733f012e66f07af4
MarcinGorcz <gorczyca@student.agh.edu.pl>
2020-11-22 22:25:38 +0100

    Client added, script detecting hardware added

---
+++ client.py
@@ -0,0 +1,19 @@
+login = "user1"
+password = ""
+ip = "34.65.196.190"
+
+
+#Please install sshpass!
+#sudo apt-get install sshpass
+
+def run_command_shell(command):
+    import subprocess
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,shell=True)
+    output, error = process.communicate()
+    if output:
+        print(output)
+    if error:
+        print(error)
+
+def dd_to_server():
+    dd_cmd = "dd if=/dev/sda | gzip -1 - | sshpass -p "+password+" ssh "+login+"@"+ip+"34.65.196.190"" dd of=image.gz"
---
+++ get_system_info.py
@@ -0,0 +1,75 @@
+import psutil
+import platform
+
+class parametry_komputera:
+    def __init__(self):
+        uname = platform.uname()
+
+        #SYSTEM
+        self.system_operacyjny = uname.system
+        self.wersja_systemu = uname.version
+
+        #CPU
+        cpufreq = psutil.cpu_freq()
+        self.rdzenie_fizyczne = psutil.cpu_count(logical=False)
+        self.rdzenie_logiczne = psutil.cpu_count(logical=True)
+        self.cpufreq = cpufreq.min
+
+        #RAM
+        svmem = psutil.virtual_memory()
+        self.ram = svmem.total
+
+        #Pamiec
+        self.dyski = {}
+        for partition in psutil.disk_partitions():
+            self.dyski[str(partition.device)] = psutil.disk_usage(partition.mountpoint).used
+            #partition_usage.total
+        #PODSUMOWANIE:
+        self.tabela_systemu = {
+            "SystemOperacyjny":self.system_operacyjny,
+            "WersjaSystemu":self.wersja_systemu,
+            "IloscRdzeniFizycznych":self.rdzenie_fizyczne,
+            "IloscRdzeniLogicznych":self.rdzenie_logiczne,
+            "TaktowanieRdzeni":self.cpufreq,
+            "IloscRamu":self.ram,
+            "Dyski":self.dyski,
+        }
+
+    def dopasuj_maszynę(self):
+        typy_tl = [
+            {"type" : "e2-standard-2","cpu": 2, "memory": 8},
+            {"type" : "e2-standard-4","cpu": 4, "memory": 16},
+            {"type" : "e2-standard-8","cpu": 8, "memory": 32},
+            {"type" : "e2-standard-16","cpu": 16, "memory": 64},
+            {"type" : "e2-standard-32","cpu": 32, "memory": 128},
+        ]
+        ram = self.tabela_systemu["IloscRamu"]/(1024*1024*1024)
+        cpu = 1
+        #cpu = self.tabela_systemu["IloscRdzeniLogicznych"]
+        wybrany_typ=""
+
+        for tl in typy_tl:
+            if(int(tl["cpu"])>int(cpu) and float(tl["memory"])>float(ram)):
+                wybrany_typ = tl["type"]
+                break
+        if(wybrany_typ == ""):
+            wybrany_typ= "e2-standard-32"
+        self.tabela_systemu["ZalecanyVM"] = wybrany_typ
+        print(wybrany_typ)
+
+    def printuj_system(self):
+        print(self.tabela_systemu)
+
+    def konwertuj_do_VHD(self):
+        if(self.tabela_systemu == "Windows"):
+            pass
+        if(self.tabela_systemu == "Linux"):
+            pass
+        else:
+            raise Exception("Sorry your system is not supported")
+        pass
+
+
+Linux = parametry_komputera()
+Linux.printuj_system()
+Linux.dopasuj_maszynę()
