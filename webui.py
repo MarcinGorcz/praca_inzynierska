87c6dfe502cdb65350859e4ce10d24b11adcf36c
MarcinGorcz <gorczyca@student.agh.edu.pl>
2020-11-30 11:47:11 +0100

    Webui settings panel added

--- webui.py
+++ webui.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 from create_vm import create_vm
 from create_vm import validate_user
 
+passphrase = ""
 
 @app.route('/')
 def index():
@@ -18,12 +19,18 @@ def my_form_post():
     login = request.form['text']
     password = request.form['password']
     if (validate_user(password, login)):
-        output = create_vm(password)
-        output1 = output.split("STATUS", 1)[0]
-        output2 = output.split("STATUS", 1)[1]
-        return render_template('index.html', output_1=output1, output_2=output2)
+        passphrase = password
+        return render_template('setting_page.html')
     else:
         return render_template('index.html', invalidcode = "Nie ma takiego uzytkownika")
+
+
+@app.route('/', methods=['POST'])
+def my_vm_button():
+    output = create_vm(passphrase)
+    output1 = output.split("STATUS", 1)[0]
+    output2 = output.split("STATUS", 1)[1]
+    return render_template('setting_page.html', output_1=output1, output_2=output2)
 
 
 if __name__ == '__main__':
